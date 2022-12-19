cdce08e4e25db0ac14e139692c6cbdf39267d551
Stefan Pantelic <112242056+StefanPantelic84@users.noreply.github.com>
2022-12-20 00:56:52 +0100

    some anim

--- components/firstvisit/FirstVisitOne.js
+++ components/firstvisit/FirstVisitOne.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useInView } from 'react-intersection-observer'
-
+ 
 
 function FirstVisitOne() {
 
--- components/smile/SmileDark.js
+++ components/smile/SmileDark.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import { useInView } from 'react-intersection-observer'
 
 function SmileDark() {
+
+    const darkB = useInView({triggerOnce:true})
+
     return (
         <div className='smiledark-container'>
             <div className='smiledark-topimg'></div>
-            <div  className='smiledark-text'><p>"Najbolji deo onoga sto radim je kad vidim kako dentalna estetika moze da transformise osobu - njeno samopouzdanje, zdravlje i pogled na zivot. Veoma sam ponosna na nacin na koji moj tim i ja tretiramo nase pacijente."<span className="span"><br></br>-Nikola V.</span></p></div>
+            <div  className='smiledark-text'><p>"Najbolji deo onoga sto radim je kad vidim kako dentalna estetika moze da transformise osobu - njeno samopouzdanje, zdravlje i pogled na zivot. Veoma sam ponosna na nacin na koji moj tim i ja tretiramo nase pacijente."<span ref={darkB.ref} className={darkB.inView ? "span-b" : 'span-B'}><br></br>-Nikola V.</span></p></div>
             <div className='smiledark-botimg'></div>
         </div>
   )
