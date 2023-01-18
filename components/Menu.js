a2381867dfb3ec481b732bd70ecda8b6ad184196
Stefan Pantelic <112242056+StefanPantelic84@users.noreply.github.com>
2023-01-18 15:37:16 +0100

    new image

--- components/Menu.js
+++ components/Menu.js
@@ -8,9 +8,10 @@ function Menu({menuClose}) {
         <ul className='menu-paths'>
             <Link href="/" onClick={menuClose}><li>POČETNA</li></Link>
             <Link href="/O_nama" onClick={menuClose}><li>O NAMA</li></Link>
-            <Link href="/Pre_i_posle" onClick={menuClose}><li>PRE I POSLE</li></Link> 
+        
             <Link href="/Prva_poseta" onClick={menuClose}><li>VAŠA PRVA POSETA</li></Link>
             <Link href="/Najlepsi_osmeh" onClick={menuClose}><li>NAJLEPŠI OSMEH</li></Link>
+            <Link href="/Pre_i_posle" onClick={menuClose}><li>PRE I POSLE</li></Link> 
             <Link href="/Iskustva" onClick={menuClose}><li >ISKUSTVA</li></Link> 
             <Link href="/Cenovnik" onClick={menuClose}><li>CENOVNIK</li></Link>
             <Link href="/Articles" onClick={menuClose}><li>ČLANCI</li></Link>
